033c3674050dd981f1a3a1dd47f6d0c8624bfdb3
Zeroitrayo <dhaivatrpatel@gmail.com>
2026-05-07 18:40:55 +0530

    add

--- src/utils/auth/user-cache.js
+++ src/utils/auth/user-cache.js
@@ -1,5 +1,5 @@
 const USER_DISPLAY_NAME_KEY = "stuffsy_user_display_name";
-export const USER_DISPLAY_NAME_CHANGED_EVENT = "stuffsy:user-display-name-changed";
+xxexport const USER_DISPLAY_NAME_CHANGED_EVENT = "stuffsy:user-display-name-changed";
 
 const notifyUserDisplayNameChanged = (fullName) => {
   window.dispatchEvent(
