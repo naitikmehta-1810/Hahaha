acd0f498afed8f4421652e89879d26ca4526a856
naitikmehta-10 <mehtanaitik2004@gmail.com>
2026-05-08 14:45:30 +0530

    checkout_fix

--- src/components/Checkout/Shipping.jsx
+++ src/components/Checkout/Shipping.jsx
@@ -19,9 +19,7 @@ const Shipping = () => {
 
           <div className="relative">
             <select className="w-full bg-gray-1 rounded-md border border-gray-3 text-dark-4 py-3 pl-5 pr-9 duration-200 appearance-none outline-none focus:border-transparent focus:shadow-input focus:ring-2 focus:ring-blue/20">
-              <option value="0">Australia</option>
-              <option value="1">America</option>
-              <option value="2">England</option>
+              <option value="India">India</option>
             </select>
 
             <span className="absolute right-4 top-1/2 -translate-y-1/2 text-dark-4">
