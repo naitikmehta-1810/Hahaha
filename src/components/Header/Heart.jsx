d247a0d76afb6406537115dbdfb0acf0f085788c
naitikmehta-10 <mehtanaitik2004@gmail.com>
2026-05-11 21:37:49 +0530

    sdsc

--- src/components/Header/Heart.jsx
+++ src/components/Header/Heart.jsx
@@ -3,14 +3,13 @@ import React from "react";
 const Heart = ({ className = "" }) => (
   <svg
     className={className}
-    width="28"
-    height="28"
     viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
   >
     <path
-      d="M3.172 5.172a4 4 0 015.656 0L12 8.343l3.172-3.171a4 4 0 115.656 5.656L12 21.657 3.172 10.828a4 4 0 010-5.656z"
+      d="M20.5 8.75C20.5 14.25 12 19 12 19S3.5 14.25 3.5 8.75C3.5 6.4 5.32 4.5 7.58 4.5C9.02 4.5 10.29 5.25 11 6.39C11.71 5.25 12.98 4.5 14.42 4.5C16.68 4.5 20.5 6.4 20.5 8.75Z"
       stroke="currentColor"
       strokeWidth="1.8"
       strokeLinecap="round"
